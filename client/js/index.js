e9596da40cd5b4faa04ab4bd5a6275630657a045
Graham Barber <grahammbarber@gmail.com>
2017-09-07 16:18:58 -0700

    Added app index with routing to views

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,25 @@
 import '../sass/vars.sass'
+
+import {h, app} from 'hyperapp'
+import hyperx from 'hyperx'
+import {router} from '@hyperapp/router'
+
+import state from './state'
+
+import Home from './views/home'
+import Info from './views/info'
+import Stats from './views/stats'
+import Comments from './views/comments'
+
+const html = hyperx(h)
+
+app({
+  state,
+  mixins: [router()],
+  view: [
+    ['/', Home],
+    ['/info', Info],
+    ['/stats', Stats],
+    ['/comments', Comments]
+  ]
+})
