35333b1787b3828a26c643cdfcd747d1666f61ae
Graham Barber <grahammbarber@gmail.com>
2017-09-07 16:49:19 -0700

    Added basic data for testing routing

---
+++ client/js/views/comments.js
@@ -0,0 +1,12 @@
+import {h} from 'hyperapp'
+import hyperx from 'hyperx'
+
+import NavBar from '../components/navbar'
+
+const html = hyperx(h)
+
+const Comments = () => {
+  return html`<p class="title">The comments page!</p>`
+}
+
+export default Comments
---
+++ client/js/views/stats.js
@@ -0,0 +1,12 @@
+import {h} from 'hyperapp'
+import hyperx from 'hyperx'
+
+import NavBar from '../components/navbar'
+
+const html = hyperx(h)
+
+const Stats = () => {
+  return html`<p class="title">The stats page!</p>`
+}
+
+export default Stats
