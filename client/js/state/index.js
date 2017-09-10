37fec9f69866336394198d5be9af24e4df087760
Graham Barber <grahammbarber@gmail.com>
2017-09-09 19:34:57 -0700

    Removed spare line, added example comment data

--- client/js/state/index.js
+++ client/js/state/index.js
@@ -2,5 +2,27 @@ export default {
   title: 'When It Counts',
   eventNum: '39',
   theme: 'Running out of Power',
-  ldjam: 'https://ldjam.com/events/ludum-dare/39/when-it-counts'
+  ldjam: 'https://ldjam.com/events/ludum-dare/39/when-it-counts',
+  comments: [
+    {
+      name: 'Jason Kennedy',
+      text: 'more of an interactive fiction then a game. but a very interesting interactive fiction',
+      info: {
+        handle: '@jason-kennedy',
+        time: '5 weeks ago',
+        likes: 1,
+        photo: 'https://static.jam.vg/other/dummy/user64.png'
+      }
+    },
+    {
+      name: 'Angela He',
+      text: 'This was a very interesting story :D They all want to say my name!! I like how the NPCs have unique characters and you can learn more about yourself by talking to them.',
+      info: {
+        handle: '@angela-he',
+        time: '5 weeks ago',
+        likes: 1,
+        photo: 'https://static.jam.vg/content/241/8/z/4d0f.jpg'
+      }
+    }
+  ]
 }
