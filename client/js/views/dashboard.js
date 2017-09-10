a13ee6724be44f8952b903c687729e9b32227557
Graham Barber <grahammbarber@gmail.com>
2017-09-09 22:04:09 -0700

    Added and implemented Details (not Description) component

--- client/js/views/dashboard.js
+++ client/js/views/dashboard.js
@@ -4,6 +4,7 @@ import hyperx from 'hyperx'
 import NavBar from '../components/navbar'
 import Stats from '../components/stats'
 import Metrics from '../components/metrics'
+import Details from '../components/details'
 
 const html = hyperx(h)
 
@@ -17,23 +18,7 @@ const Dashboard = (state, actions) => {
         ${Stats(state.stats)}
         <div class="tile is-parent is-vertical">
           ${Metrics(state.metrics)}
-          <div class="tile is-child box">
-            <h4 class="title is-4">Description</h4>
-            <p class="content">
-              <p>A game about the hero’s life after the quest. You saved the world, and now you’re back home, but your sacrifices are catching up with you. Speak with whoever matters to you most.</p>
-              <p>Arrow keys or A/D to move. Space/Enter to interact or skip.</p>
-              <p>There is no secret ending. Choose your own objective.</p>
-              <p>Made with Phaser + ES6 + Webpack.</p>
-            </p>
-            <hr>
-            <h5 class="title is-4">Links</h5>
-              <h6 class="heading">Source code</h6>
-              <a href="https://github.com/monochromicon/ludum-dare39">https://github.com/monochromicon/ludum-dare39</a>
-              <br>
-              <br>
-              <h6 class="heading">HTML5 (web)</h6>
-              <a href="https://monochromicon.github.io/ludum-dare39/">https://monochromicon.github.io/ludum-dare39/</a>
-          </div>
+          ${Details(state.description, state.links)}
           <div class="tile is-child box">
             <h4 class="title is-4">Authors</h4>
             <div class="level">
