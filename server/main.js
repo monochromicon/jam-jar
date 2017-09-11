ad89253b2f6cc18df0c641bbb97aac64ce04fed0
Graham Barber <grahammbarber@gmail.com>
2017-09-10 20:31:48 -0700

    Server provides static files + route that returns server IP

--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
-const init = () => {
-  // Do a thing
-}
+import fs from 'fs'
+
+import {send} from 'micro'
+import {router, get} from 'microrouter'
+
+import * as ip from './routes/ip' 
+
+const root = `${__dirname}/../dist`
+
+const html = fs.readFileSync(`${root}/index.html`, 'utf8')
+const css = fs.readFileSync(`${root}/styles.css`, 'utf8')
+const js = fs.readFileSync(`${root}/bundle.js`, 'utf8')
+
+const contentHtml = async (req, res) => send(res, 200, html)
+const contentCSS = async (req, res) => send(res, 200, css)
+const contentJS = async (req, res) => send(res, 200, js)
+
+const init = router(
+  get('/', contentHtml),
+  get('/styles.css', contentCSS),
+  get('/bundle.js', contentJS),
+  get('/ip', ip.get)
+)
 
 export default init
---
+++ server/routes/ip/index.js
@@ -0,0 +1,6 @@
+import {send} from 'micro'
+import ip from 'ip'
+
+export const get = async (req, res) => {
+  send(res, 200, ip.address())
+}
