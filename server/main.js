1eb7aa11d4750f2d9a5b01823cb8efbc01dcdb4d
Graham Barber <grahammbarber@gmail.com>
2017-09-11 09:05:46 -0700

    Implemented GraphQL routes, added graphql dep

--- server/main.js
+++ server/main.js
@@ -1,25 +1,34 @@
 import fs from 'fs'
 
 import {send} from 'micro'
-import {router, get} from 'microrouter'
+import {router, get, post} from 'microrouter'
+import {microGraphiql, microGraphql} from 'apollo-server-micro'
 
-import * as ip from './routes/ip' 
+import schema from './schema'
+import * as ip from './routes/ip'
+
+const gqlHandler = microGraphql({schema})
+const giqlHandler = microGraphiql({endpointURL: '/gql'})
 
 const root = `${__dirname}/../dist`
 
-const html = fs.readFileSync(`${root}/index.html`, 'utf8')
-const css = fs.readFileSync(`${root}/styles.css`, 'utf8')
 const js = fs.readFileSync(`${root}/bundle.js`, 'utf8')
+const css = fs.readFileSync(`${root}/styles.css`, 'utf8')
+const html = fs.readFileSync(`${root}/index.html`, 'utf8')
 
-const contentHtml = async (req, res) => send(res, 200, html)
-const contentCSS = async (req, res) => send(res, 200, css)
 const contentJS = async (req, res) => send(res, 200, js)
+const contentCSS = async (req, res) => send(res, 200, css)
+const contentHtml = async (req, res) => send(res, 200, html)
 
 const init = router(
   get('/', contentHtml),
   get('/styles.css', contentCSS),
   get('/bundle.js', contentJS),
-  get('/ip', ip.get)
+  get('/ip', ip.get),
+  get('/gql', gqlHandler),
+  post('/gql', gqlHandler),
+  get('/giql', giqlHandler),
+  async (req, res) => send(res, 404, 'Try somewhere else, my friend')
 )
 
 export default init
